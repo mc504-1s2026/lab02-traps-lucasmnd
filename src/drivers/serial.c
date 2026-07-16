bc812358c7b918a2921cfcdf69ac6b788a58a8c3
lucasmnd <lucasrodriguesdemendonca@gmail.com>
2026-07-15 23:36:41 -0300

    Mayhaps works?

--- src/drivers/serial.c
+++ src/drivers/serial.c
@@ -1,44 +1,81 @@
 #include <kernel/serial.h>
 #include <kernel/panic.h>
+#include "arch/csr.h"
+#include "arch/io.h"
+#include "arch/plic.h"
+#include "arch/spinlock.h"
+
+#define SERIAL_BUF_SIZE 256
+
+struct serialdev {
+    /* buffer storing received serial data */
+    char buf[SERIAL_BUF_SIZE]; 
+    /* number of bytes stored in this buffer */
+    size_t len;
+	struct spinlock sl;
+} dev;
 
 void serial_init()
 {
-	/* not implemented */
-	BUG();
+	spin_init(&dev.sl);
+	dev.len = 0;
+	void *addr_queue = SERIAL_BASE + SERIAL_FCR;
+	u8 flags = SERIAL_FCR_FIFO_ENABLE | SERIAL_FCR_RX_FIFO_CLEAR | SERIAL_FCR_TX_FIFO_CLEAR;
+	iowrite8(flags, addr_queue);
 }
 
 void serial_irq_enable()
 {
-	/* not implemented */
-	BUG();
+	iowrite8(SERIAL_IER_ERBFI, SERIAL_BASE + SERIAL_IER);
+	plic_irq_set_priority(IRQ_SERIAL, 1);
+	plic_hart_set_threshold(0, 0);
+	plic_hart_enable_irq(0, IRQ_SERIAL);
+	csr_set(CSR_SIE, CSR_SIE_SEIE);
 }
 
 void serial_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	iowrite8(0, SERIAL_BASE + SERIAL_IER);
+	plic_irq_set_priority(IRQ_SERIAL, 0);
+	csr_clear(CSR_SIE, CSR_SIE_SEIE);
 }
 
 void serial_irq()
 {
-	/* not implemented */
-	BUG();
+	spin_lock(&dev.sl);
+	if(dev.len < SERIAL_BUF_SIZE){
+		dev.buf[dev.len++] = ioread8(SERIAL_BASE + SERIAL_RBR);
+	}
+	spin_unlock(&dev.sl);
 }
 
 size_t serial_read(char *buf)
 {
-	/* not implemented */
-	BUG();
+	u64 temp = spin_lock_irqsave(&dev.sl);
+
+	for(unsigned int i = 0; i < dev.len; i++){
+		buf[i] = dev.buf[i];
+	}
+
+	size_t ret = dev.len;
+	dev.len = 0;
+
+	spin_unlock_irqrestore(&dev.sl, temp);
+
+	return ret;
 }
 
 void serial_puts(char *str)
 {
-	/* not implemented */
-	BUG();
+	while(str[0] != '\0'){
+		serial_putc(str[0]);
+		str++;
+	}
 }
 
 void serial_putc(char c)
 {
-	/* not implemented */
-	BUG();
+	while((ioread8(SERIAL_BASE + SERIAL_LSR) & SERIAL_LSR_THRE) == 0);
+
+	iowrite8(c, SERIAL_BASE + SERIAL_THR);
 }
--- src/kmain.c
+++ src/kmain.c
@@ -3,8 +3,10 @@
 #include <arch/timer.h>
 #include <kernel/trap.h>
 #include <kernel/serial.h>
+#include "kernel/string.h"
 
 extern int _hartid[];
+
 void kmain()
 {
 	printk_set_level(LOG_DEBUG);
@@ -21,5 +23,47 @@ void kmain()
 	serial_init();
 	serial_irq_enable();
 
-	/* implement your shell here */
+	char command_buf[256], input_buf[256];
+	size_t command_index = 0;
+
+	serial_puts("Shell initialized!\r\n");
+
+	while(1){
+		serial_puts("> ");
+
+		size_t read_bytes = serial_read(input_buf);
+
+		for(unsigned long i = 0; i < read_bytes; i++){
+			char current_char = input_buf[i];
+
+			serial_putc(input_buf[i]);
+
+			if(current_char == '\r'){
+				serial_putc('\n');
+				command_buf[command_index + i] = '\0';
+
+				char output[64];
+
+				if(strncmp("uptime", command_buf, 6) == 0){
+					snprintf(output, sizeof(output), "%ds\r\n", timer_read());
+					serial_puts(output);
+				} else if(strncmp("echo ", command_buf, 5) == 0){
+					serial_puts(&command_buf[5]);
+					serial_puts("\r\n");
+				} else if(strncmp("alarm ", command_buf, 6) == 0){
+					u64 seconds = strtou64(&command_buf[6], 10);
+					timer_set_alarm(seconds);
+					timer_irq_enable();
+				} else {
+					snprintf(output, sizeof(output), "Unknown Command: %s\r\n", command_buf);
+					serial_puts(output);
+				}
+
+
+			} else {
+				command_buf[command_index + i] = current_char;
+			}
+		}
+		command_index += read_bytes;
+	}
 }
